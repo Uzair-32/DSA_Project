e6f6a3758787392d9c23592af3ed9e2ed02fc3ad
Muhammad Uzair <muhammaduzair32323@gmail.com>
2025-12-22 13:04:19 +0500

    Delete Source directory

--- Source/project_goldfish/project_goldfish.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "project_goldfish.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_goldfish, "project_goldfish" );
- 
